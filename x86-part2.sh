9c2056d261baf0e3604df8ec4af07f6358de81cf
tom wang <jimoyuanfeng@gmail.com>
2020-12-01 22:54:47 +0800

    Update x86-part2.sh

--- x86-part2.sh
+++ x86-part2.sh
@@ -12,3 +12,4 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/lean/luci-theme-atmaterial
