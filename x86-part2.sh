b931865d36938f7d603555d9477adea192cbf3d2
tom wang <jimoyuanfeng@gmail.com>
2020-12-03 10:53:37 +0800

    Update x86-part2.sh

--- x86-part2.sh
+++ x86-part2.sh
@@ -13,6 +13,7 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/holmium/1715173329/g' package/lean/dnsforwarder/Makefile
+sed -i 's/6fa74a3fc87cba97dbc4a7213b760f8d997cd9c5f11900d47d387b23764cf20a/6a95c4323e008499af8d3549513c8a128c69b97296888a4c4b642e6c14a65508/g' package/firmware/ath10k-firmware/Makefile
 rm -rf ./package/lean/v2ray
 rm -rf ./package/lean/v2ray-plugin
 rm -rf ./package/lean/luci-theme-argon
