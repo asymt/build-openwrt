08437f82f38ddfe576d7f0d191e97f37f031f205
tom wang <jimoyuanfeng@gmail.com>
2020-12-03 21:20:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,18 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/holmium/1715173329/g' package/lean/dnsforwarder/Makefile
+sed -i 's/6fa74a3fc87cba97dbc4a7213b760f8d997cd9c5f11900d47d387b23764cf20a/6a95c4323e008499af8d3549513c8a128c69b97296888a4c4b642e6c14a65508/g' package/firmware/ath10k-firmware/Makefile
+sed -i 's/68e92820c51270eba4f68b24654c4a9408902b2600762b70204f4cb5419bb714/baa971a9cb2be3415c6aee06aff4ec59246c85f289b87bf1fcf69041495fb07c/g' package/firmware/ath10k-firmware/Makefile
+rm -rf ./package/lean/v2ray
+rm -rf ./package/lean/v2ray-plugin
+rm -rf ./package/lean/luci-theme-argon
+rm -rf ./package/lienol/v2ray
+rm -rf ./package/lienol/v2ray-plugin
+rm -rf ./feeds/packages/net/smartdns
+rm -rf ./feeds/packages/net/https-dns-proxy
+rm -rf ./feeds/packages/lang/golang
+
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray package/lean/v2ray
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray-plugin package/lean/v2ray-plugin
+svn co https://github.com/coolsnowwolf/packages/trunk/lang/golang feeds/packages/lang/golang
