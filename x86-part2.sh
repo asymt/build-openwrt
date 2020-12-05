254b960bb1e4762253bc7243c4ec5cea33dd5c42
tom wang <jimoyuanfeng@gmail.com>
2020-12-05 11:17:34 +0800

    Update x86-part2.sh

--- x86-part2.sh
+++ x86-part2.sh
@@ -26,6 +26,7 @@ rm -rf ./feeds/packages/net/smartdns
 rm -rf ./feeds/packages/net/https-dns-proxy
 rm -rf ./feeds/packages/lang/golang
 rm -rf tools/cmake
+rm -rf package/lienol/lang/golang
 
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray package/lean/v2ray
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray-plugin package/lean/v2ray-plugin
