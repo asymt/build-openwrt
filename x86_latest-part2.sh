027d5d2031843f20d167dcd8c7d906c7a43da04e
tom wang <jimoyuanfeng@gmail.com>
2020-12-06 11:25:17 +0800

    Update x86_latest-part2.sh

--- x86_latest-part2.sh
+++ x86_latest-part2.sh
@@ -13,6 +13,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
 sed -i 's/PKG_SOURCE_URL\S*/PKG_SOURCE_URL:=https:\/\/github.com\/AdguardTeam\/AdGuardHome\/releases\/download\/v0.104.3/g' feeds/diy/adguardhome/Makefile 
+sed -i 's/=86/=386/g' feeds/diy/adguardhome/Makefile
 pushd package/OpenClash/luci-app-openclash/tools/po2lmo
 make && sudo make install
 popd
